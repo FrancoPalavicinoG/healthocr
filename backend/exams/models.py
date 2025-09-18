408522c6003d982db8e0997c0ba89812693bb6a8
Franco Palavicino <fppalavicino@miuandes.cl>
2025-09-18 12:36:54 -0300

    feat(models): exam results model, each row in this table represents an individual value for a exam test

--- backend/exams/models.py
+++ backend/exams/models.py
@@ -14,3 +14,23 @@ class Exam(models.Model):
 
     def __str__(self) -> str:
         return f"Exam {self.id} - {self.created_at.isoformat()}"
+    
+class ExamResult(models.Model):
+    """
+    Cada fila de esta tabla representa un valor individual de un examen.
+    Ejemplo: Glucosa 90 mg/dL
+    """
+    exam = models.ForeignKey(
+        Exam,
+        on_delete=models.CASCADE, 
+        related_name="results"      # Permite acceder con exam.results.all()
+    )
+    test_name = models.CharField(max_length=255) 
+    value = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=50, null=True, blank=True)
+    reference_range = models.CharField(max_length=100, null=True, blank=True)
+    method = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.test_name}: {self.value} {self.unit}"
