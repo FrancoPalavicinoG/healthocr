1a4de0ae4315a9e8c2b699c706f41e10ac1be506
Franco Palavicino <fppalavicino@miuandes.cl>
2025-09-18 17:50:08 -0300

    feat(serializers): exam results serializer

--- backend/exams/serializers.py
+++ backend/exams/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Exam
+from .models import Exam, ExamResult
 
 # Definimos un serializer para el modelo Exam. 
 # Un serializer convierte un modelo en JSON para la API y valida datos de entrada 
@@ -8,3 +8,9 @@ class ExamSerializer(serializers.ModelSerializer):
         model = Exam
         fields = ['id', 'image', 'result_text', 'created_at']
         read_only_fields = ['id', 'result_text', 'created_at']
+
+class ExamResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamResult
+        field = ['id', 'test_name', 'value', 'unit', 'reference_range', 'created_at']
+        read_only_fields = ['id', 'created_at']
